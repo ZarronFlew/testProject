15a177be7cf9a84a7d6a3db075e9ec7fea719058
Zarron <kenes.zaron@mail.ru>
2023-06-30 19:03:46 +0600

    fixed little file menagment

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function() {
+
+eval("let burger = document.querySelector('.burger');\r\nlet bg = document.querySelector('.dark-bg');\r\nlet mobileMenu = document.querySelector('.mobile-menu');\r\n\r\nburger.addEventListener('click', function () {\r\n    burger.classList.toggle('open');\r\n    mobileMenu.classList.toggle('open');\r\n    bg.classList.toggle('open');\r\n})\r\n\n\n//# sourceURL=webpack://mygulp/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
